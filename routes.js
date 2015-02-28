511da5fefa819d38b301e3fa71a14b96133b8635
Oliver <o.lucas@live.com>
2015-02-28 18:16:00 +0000

    finished off the routes.js file with avatar, room, room limit, joining and leaving the room, handling messages, and user knowing how many are in the room

--- routes.js
+++ routes.js
@@ -52,7 +52,108 @@ var chat = io.off('/socket').on('connection', function (socket) {
           id: data
         });
       }
-    });
-});
+      else if(roon.length >= 2) {
 
+        chat.emit('tooMany', {boolean: true});
+      }
+    });
+
+    // When the client emits 'login', save his name and avatar,
+    // and add them to the room
+    socket.on('login', function(data) {
+
+      var room = findClientsSocket(io, data.id, '/socket');
+      // Only 2 people per room are allowed in this example
+      // This functionality will be removed for the project it is intended for
+      if (room.length < 2) {
+
+        // Use the socket object to store data.
+        // Each client gets their own unique socket object
+
+        socket.username = data.user;
+        socket.room = data.id;
+        socket.avatar = gravatar.url(data.avatar, {s: '140', r: 'x', d: 'mm'});
+
+        // Tell the person what he should use for an avatar
+        socket.emit('img', socket.avatar);
+
+        //Add the client to the room
+        socket.join(data.id);
+
+        if (room.length == 1) {
+
+          var usernames = [],
+            avatars = [];
+
+          usernames.push(room[0].username);
+          usernames.push(socket.username);
+
+          avatars.push(room[0].avatar);
+          avatars.push(socket.avatar);
+
+          // Send the startChat event to all people in the
+          // room, along with a list of people that are in it.
+
+          chat.in(data.id).emit('startChat', {
+            boolean: true,
+            id: data.id,
+            users: usernames,
+            avatars: avatars
+          });
+        }
+      }  
+      else {
+        socket.emit('tooMany', {boolean: true});
+      }
+    });
+    
+    //Somebody left the chat
+    socket.on('disconnect', function(){
+
+      // Notify the other person in the chat room
+      // that his chat partner has left
+
+      socket.broadcast.to(this.room).emit('leave', {
+        boolean: true,
+        room: this.room
+        user: this.username
+        avatar: this.avatar
+      });
+
+      // leave the room
+      socket.leave(socket.room);
+    });
+
+    // Handle the sending of messages
+    socket.on('msg', function(data){
+
+      // When the server receives a message, it sends it
+      // to the other person in the room
+      socket.broadcast.to(socket.room).emit('receieve', {
+        msg: data.msg,
+        user: data.user
+        img: data.img
+      })
+    });  
+  });
 };
+
+function findClintsSocket(io, roomId, namespace) {
+  var res = [],
+    ns = io.of(namespace ||"/");  // the default namespace is "/"
+
+  if (ns) {
+    for (var id in ns.connected) {
+      if(roomId) {
+        var index = ns.connected[id].rooms.indexOf(roomId);
+        if(index !==-1) {
+          res.push(ns.connected[id]);
+        }
+      }
+      else{
+        res.push(ns.connected[id]);
+      }
+    }
+  }
+  return res;
+}
